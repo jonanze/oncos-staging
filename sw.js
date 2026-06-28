6a0190b268c0f9311b186f7728a30322db08e856
Jonan <jonantanzhien@gmail.com>
2026-06-28 21:40:48 +0800

    Update staging reference 2026-06-28

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = 'staging-2026-06-28';
+const CACHE = 'staging-598bf2add0';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+  e.respondWith(fetch(e.request).then(resp => {
     const cp = resp.clone(); caches.open(CACHE).then(c => c.put(e.request, cp)); return resp;
-  }).catch(() => caches.match('./index.html'))));
+  }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html'))));
 });
