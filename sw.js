f5527ead6b695680f5b5a46565b8ad2d0a6aad47
Jonan <jonantanzhien@gmail.com>
2026-07-01 17:03:01 +0800

    Update staging reference 2026-07-01

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'staging-400c427039';
+const CACHE = 'staging-b977274991';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
